eb77255987acf7929be785d9111f55c475774749
professorArana <31639021+professorArana@users.noreply.github.com>
2018-03-27 02:16:56 -0700

    env url test
    env url test

--- src/main/java/com/example/microservicesbasics/kittens/web/KittensWeb.java
+++ src/main/java/com/example/microservicesbasics/kittens/web/KittensWeb.java
@@ -1,17 +1,21 @@
 package com.example.microservicesbasics.kittens.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KittensWeb {
+	@Value("${kittensUrl:default.url}")
+	private String kittensUrl;
+	
 	@RequestMapping("/kittens")
 	@ResponseBody
 	KittensWebResult kittens() {
 		KittensWebResult kwRes = new KittensWebResult();
 		kwRes.setId("1");
-		kwRes.setName("kitty");
+		kwRes.setName("kitty "+kittensUrl);
 		return kwRes;
 	}
 }
